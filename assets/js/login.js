04834dcd8ee11d8765d4b6804cb6e171cc783321
FX-at-Dev <88873925+FX-at-Dev@users.noreply.github.com>
2024-12-17 11:55:01 +0530

    Add files via upload

---
+++ assets/js/login.js
@@ -0,0 +1,24 @@
+// Get the login form
+const loginForm = document.getElementById('login-form');
+
+// Add event listener for form submission
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent default form submission behavior
+
+  // Get username and password values
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // Validate credentials
+  if (username === 'stuadmin' && password === 'stu123') {
+    // Redirect to Student Dashboard
+    window.location.href = '/Student/student-dashboard.html';
+  } else if (username === 'admin' && password === 'admin') {
+    // Redirect to Teacher Dashboard
+    window.location.href = '/Teacher/teacher-dashboard.html';
+  } else {
+    // Display an error message
+    alert('Invalid username or password. Please try again.');
+  }
+});
+
